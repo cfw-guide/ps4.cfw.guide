b49018f8c157e0750ba04852c4ad3ef3602738bb
emiyl <me@emiyl.com>
2021-12-14 21:28:31 +0000

    added sidebar

--- docs/.vuepress/i18n/en_US/sidebar.js
+++ docs/.vuepress/i18n/en_US/sidebar.js
@@ -1,1 +1,14 @@
-module.exports = {}
+module.exports = {
+  '/': [
+    {
+      text: 'Guide',
+      children: [
+        '/',
+        '/get-started',
+        '/updating-to-9.00',
+        '/installing-poobs4',
+        '/installing-goldhen',
+      ],
+    },
+  ],
+}
